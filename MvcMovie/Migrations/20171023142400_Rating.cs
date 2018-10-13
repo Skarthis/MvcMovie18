f6fe9f4d2e6e81675676ff49a227cc7d2af7357c
Skarthis <32575517+Skarthis@users.noreply.github.com>
2018-10-13 18:02:53 +0100

    Add project files.

---
+++ MvcMovie/Migrations/20171023142400_Rating.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace MvcMovie.Migrations
+{
+    public partial class Rating : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "price",
+                table: "Movie",
+                newName: "Price");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Rating",
+                table: "Movie",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Movie");
+
+            migrationBuilder.RenameColumn(
+                name: "Price",
+                table: "Movie",
+                newName: "price");
+        }
+    }
+}
